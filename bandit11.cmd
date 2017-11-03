c7cfe53a5a9d5c8b8df02267736dd3ee212de617
Mark <mark@V-TRIES5-SP3.northamerica.corp.microsoft.com>
2017-11-03 15:33:57 +0100

    initial commit level12

---
+++ bandit11.cmd
@@ -0,0 +1,5 @@
+# rot 13
+# https://exploitshit.wordpress.com/2015/04/25/decode-rot13-on-linux-command-line/
+cat data.txt | tr '[a-zA-Z]' '[n-za-mN-ZA-m]'
+
+
---
+++ bandit6.cmd
@@ -0,0 +1,2 @@
+find / -size 33c -group bandit6 -user bandit7 2>/dev/null
+
